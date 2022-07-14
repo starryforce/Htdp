0398e59a15b26f2b0dbd16e11d5cf85e033afee1
starryforce <starryforce@gmail.com>
2022-07-14 23:50:28 +0800

    2022/07/14

---
+++ chapter1-6/6.1.rkt
@@ -0,0 +1,46 @@
+#lang htdp/bsl
+
+(require 2htdp/image)
+
+(define TANK-WIDTH 40)
+(define TANK-HEIGHT 20)
+(define TANK (rectangle TANK-WIDTH TANK-HEIGHT "solid" "brown"))
+
+(define MISSILE-SIDE (/ TANK-WIDTH 2))
+(define MISSILE (triangle MISSILE-SIDE "solid" "red"))
+
+(define UFO (overlay (circle 10 "solid" "green")
+                     (ellipse 60 30 "solid" "grey")))
+
+
+(define BACKGROUND-WIDTH 400)
+(define BACKGROUND-HEIGHT 800)
+(define BACKGROUND (rectangle BACKGROUND-WIDTH BACKGROUND-HEIGHT "solid" "royalblue"))
+
+(define-struct aim [ufo tank])
+(define-struct fired [ufo tank missile])
+
+
+; A UFO is a Posn. 
+; interpretation (make-posn x y) is the UFO's location 
+; (using the top-down, left-to-right convention)
+ 
+(define-struct tank [loc vel])
+; A Tank is a structure:
+;   (make-tank Number Number). 
+; interpretation (make-tank x dx) specifies the position:
+; (x, HEIGHT) and the tank's speed: dx pixels/tick 
+ 
+; A Missile is a Posn. 
+; interpretation (make-posn x y) is the missile's place
+
+
+; SIGS -> Image
+; adds TANK, UFO, and possibly MISSILE to 
+; the BACKGROUND scene
+(define (si-render s)
+  (cond
+    [(aim? s) (... (aim-tank s) ... (aim-ufo s) ...)]
+    [(fired? s) (... (fired-tank s) ... (fired-ufo s) ... (fired-missile s) ...)]))
+
+ 
