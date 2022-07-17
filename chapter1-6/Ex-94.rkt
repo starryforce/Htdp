7e3591c40fe99c16ac1c4265c041c42f0dcf2db2
starryforce <starryforce@gmail.com>
2022-07-17 14:20:53 +0800

    2022/07/16

--- chapter1-6/Ex-94.rkt
+++ chapter1-6/Ex-94.rkt
@@ -16,3 +16,20 @@
 (define BACKGROUND-WIDTH 400)
 (define BACKGROUND-HEIGHT 800)
 (define BACKGROUND (rectangle BACKGROUND-WIDTH BACKGROUND-HEIGHT "solid" "royalblue"))
+
+
+(define-struct aim [ufo tank])
+(define-struct fired [ufo tank missile])
+
+; A UFO is a Posn. 
+; interpretation (make-posn x y) is the UFO's location 
+; (using the top-down, left-to-right convention)
+ 
+(define-struct tank [loc vel])
+; A Tank is a structure:
+;   (make-tank Number Number). 
+; interpretation (make-tank x dx) specifies the position:
+; (x, HEIGHT) and the tank's speed: dx pixels/tick 
+ 
+; A Missile is a Posn. 
+; interpretation (make-posn x y) is the missile's place
