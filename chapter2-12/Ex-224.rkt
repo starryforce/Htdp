9085e87ff405b7373faec488bdaf743ee0c4659c
starryforce <starryforce@gmail.com>
2022-09-22 19:58:33 +0800

    2022/09/22

--- chapter2-12/Ex-224.rkt
+++ chapter2-12/Ex-224.rkt
@@ -7,21 +7,31 @@
 (define SCENE-HEIGHT 800)
 
 (define TANK (above (wedge 10 180 "solid" "grey") (rectangle 40 10 "solid" "grey")))
-(define TANK-SPEED 10)
 (define MOON (circle 20 "solid" "lightgoldenrodyellow"))
 (define SCENE (place-image/align MOON 80 40 "right" "top" (empty-scene SCENE-WIDTH SCENE-HEIGHT "dark blue")))
 
 
-; A Tank is a number
-; represent the x-coordinate of the tank
+; A Direction is one of:
+; - "left"
+; - "right"
 
-(define tank1 0)
-(define tank2 200)
+(define-struct tank [position direction])
+; A Tank is a structure:
+; (make-tank Number Direction)
+; interpretation (make-tank p d) represents a tank
+; current at position p moving towards d
+
+(define tank1 (make-tank 0 "left"))
+(define tank2 (make-tank 0 "right"))
+(define tank3 (make-tank (/ SCENE-WIDTH 4) "left"))
+(define tank4 (make-tank (/ SCENE-WIDTH 4) "right"))
+(define tank5 (make-tank SCENE-WIDTH "left"))
+(define tank6 (make-tank SCENE-WIDTH "right"))
 
 ; Tank -> Image
 ; render the tank t onto the background
 (define (render t) (place-image/align TANK
-                                          t
+                                          (tank-position t)
                                           SCENE-HEIGHT
                                           "middle"
                                           "bottom"
@@ -33,37 +43,32 @@
                                           "middle"
                                           "bottom"
                                           SCENE))
-(check-expect (render tank2) (place-image/align TANK
-                                          200
+(check-expect (render tank3) (place-image/align TANK
+                                          100
                                           SCENE-HEIGHT
                                           "middle"
                                           "bottom"
                                           SCENE))
 
 ; Tank -> Tank
-; Tank moves according it's current direction
+; Tank moves every tick
 (define (tock t) t)
-
-
 
 ; Tank KeyEvent -> Tank
 ; press "left" to make tank move to left,
 ; press "right" to make tank move to right,
 (define (control t ke)
-  (cond [(string=? ke "left") (max (- t TANK-SPEED) 0) ]
-        [(string=? ke "right") (min (+ t TANK-SPEED) SCENE-WIDTH)]
-        [else t]))
+  (make-tank (tank-position t) (cond [(string=? ke "left") "left"]
+                                     [(string=? ke "right") "right"]
+                                     [else (tank-direction t)]
+                                     )))
 
-(check-expect (control 0 "left") 0)
-(check-expect (control SCENE-WIDTH "right") SCENE-WIDTH)
-(check-expect (control 100 "left") (- 100 TANK-SPEED))
-(check-expect (control 100 "right") (+ 100 TANK-SPEED))
-(check-expect (control 100 "a") 100)
-
+(check-expect (control tank3 "left") (make-tank (/ SCENE-WIDTH 4) "left"))
+(check-expect (control tank3 "right") (make-tank (/ SCENE-WIDTH 4) "right"))
 
 ; Number -> Tank
 ; start game with tank at x-coordinate of initial
 (define (game-main initial)
-  (big-bang initial
+  (big-bang (make-tank initial "right")
     [on-draw render]
     [on-key control]))
