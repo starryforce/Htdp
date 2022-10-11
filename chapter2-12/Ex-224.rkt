379675835e2da3c361b1237f9bfdad36cd790ad9
starryforce <starryforce@gmail.com>
2022-10-11 19:59:48 +0800

    2022/10/11

--- chapter2-12/Ex-224.rkt
+++ chapter2-12/Ex-224.rkt
@@ -14,6 +14,12 @@
 (define MISSILE-SPEED 3)
 (define MISSILE (isosceles-triangle 20 30 "solid" "red"))
 
+(define UFO-SPEED 1)
+(define UFO (overlay/offset (ellipse 35 25 "solid" "pink") 0 10 (ellipse 60 30 "solid" "silver")))
+
+(define BOMB-SPEED (* 2 UFO-SPEED))
+(define BOMB (circle 8 "solid" "red"))
+
 
 ; A Missile is a Posn
 
@@ -40,27 +46,39 @@
 (define missile3 (make-posn 20 -8))
 ; A List-of-missiles is one of:
 ; - '()
-; (cons Missile List-of-missiles)
+; - (cons Missile List-of-missiles)
 (define lom0 '())
 (define lom1 (list missile1 missile2))
 (define lom2 (list missile1 missile3))
 
-(define-struct game [tank missiles])
+; A Ufo is a Posn
+(define ufo1 (make-posn 50 50))
+(define ufo2 (make-posn 70 90))
+
+; A List-of-ufos is one of:
+; - '()
+; - (cons UFO List-of-ufos)
+(define fleet0 '())
+(define fleet1 (list ufo1))
+(define fleet2 (list ufo1 ufo2))
+
+
+(define-struct game [tank missiles fleet])
 ; A Game is a structure:
-; (make-game Tank List-of-missiles)
-; interpretation (make-game t alom) represents
-; a tank with some missiles alom
-(define game1 (make-game tank3 lom1))
-(define game2 (make-game tank3 lom2))
+; (make-game Tank List-of-missiles List-of-ufos)
+; interpretation (make-game t alom alou) represents
+; a tank with some missiles alom, some landing ufo alou
+(define game1 (make-game tank3 lom1 fleet1))
+(define game2 (make-game tank3 lom2 fleet2))
 
 ; Game -> Image
 ; render current game state
 (define (render g)
   (render-tank (game-tank g)
                (render-missiles (game-missiles g)
-                                SCENE)))
+                                (render-ufos (game-fleet g) SCENE))))
 
-(check-expect (render game1) (render-tank tank3 (render-missiles lom1 SCENE)))
+(check-expect (render game1) (render-tank tank3 (render-missiles lom1 (render-ufos fleet1 SCENE))))
 
 ; Tank -> Image
 ; render the tank t onto the background
@@ -102,6 +120,24 @@
                                          20
                                          SCENE)))
 
+; List-of-ufos -> Image
+; put a list of ufos onto the background
+(define (render-ufos alou s)
+  (cond [(empty? alou) s]
+        [else (place-image UFO
+                           (posn-x (first alou))
+                           (posn-y (first alou))
+                           (render-ufos (rest alou) s))]))
+
+(check-expect (render-ufos fleet0 SCENE) SCENE)
+(check-expect (render-ufos fleet2 SCENE) (place-image UFO
+                                         70
+                                         90
+                                         (place-image UFO
+                                         50
+                                         50
+                                         SCENE)))
+
 ; Tank -> Tank
 ; Tank moves every tick
 (define (tock-tank t)
@@ -140,19 +176,43 @@
 (check-expect (clear-missiles lom1) lom1)
 (check-expect (clear-missiles lom2) (list missile1))
 
+; List-of-ufos -> List-of-ufos
+; Ufos move downward every tick
+(define (tock-ufos alou)
+  (cond [(empty? alou) '()]
+        [else (cons (make-posn  (posn-x (first alou))
+                                (+ (posn-y (first alou)) UFO-SPEED))
+                    (tock-ufos (rest alou)))]))
+
+(check-expect (tock-ufos fleet0) fleet0)
+(check-expect (tock-ufos fleet1) (list (make-posn 50 (+ 50 UFO-SPEED))))
+(check-expect (tock-ufos fleet2) (list (make-posn 50 51) (make-posn 70 91)))
+
+
+; List-of-ufos -> List-of-ufos
+; when ufo is less than 5, generate a ufo at random place on top
+(define (join-ufo alou) alou)
+
+
 ; Game -> Game
 ; move every missiles and tank for every clock tick
 (define (tock g)
   (make-game (tock-tank (game-tank g))
-             (clear-missiles (tock-missiles (game-missiles g)))))
+             (clear-missiles (tock-missiles (game-missiles g)))
+             (tock-ufos (game-fleet g))))
+
+
 
 (check-expect (tock game1)
               (make-game (tock-tank tank3)
-                         (tock-missiles lom1)))
+                         (tock-missiles lom1)
+                         (tock-ufos fleet1)))
 
 (check-expect (tock game2)
               (make-game (tock-tank tank3)
-                         (clear-missiles (tock-missiles lom2))))
+                         (clear-missiles (tock-missiles lom2))
+                         (tock-ufos fleet2)))
+
 
 ; Tank KeyEvent -> Tank
 ; press "left" to make tank move to left,
@@ -179,23 +239,26 @@
 (check-expect (fire game1 "k") (game-missiles game1))
 (check-expect (fire game1 " ") (cons (make-posn (/ SCENE-WIDTH 4) SCENE-HEIGHT) lom1))
 
+
 ; Game KeyEvent -> Game
 ; press "left" or "right" arrow key to move the tank
 ; press "space" to lanch a missile
 (define (control g ke)
   (make-game (control-tank (game-tank g) ke)
-             (fire g ke)))
+             (fire g ke)
+             (game-fleet g)))
 
 
-(check-expect (control game1 "left") (make-game (control-tank (game-tank game1) "left") (game-missiles game1)))
+(check-expect (control game1 "left") (make-game (control-tank (game-tank game1) "left") (game-missiles game1) (game-fleet game1)))
 (check-expect (control game1 " ") (make-game (game-tank game1)
                                              (cons (make-posn (/ SCENE-WIDTH 4) SCENE-HEIGHT)
-                                                   (game-missiles game1))))
+                                                   (game-missiles game1))
+                                             (game-fleet game1)))
 
 ; Number -> Tank
 ; start game with tank at x-coordinate of initial
 (define (game-main speed)
-  (big-bang (make-game (make-tank 0 "right") '())
+  (big-bang (make-game (make-tank 0 "right") '() '())
     [on-draw render]
     [on-key control]
     [on-tick tock speed]))
