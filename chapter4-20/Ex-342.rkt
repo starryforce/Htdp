cd2ac428a7270c946a6beebfffa7de3741f9202c
starryforce <starryforce@gmail.com>
2022-11-23 17:49:59 +0800

    2022/11/23-1

--- chapter4-20/Ex-342.rkt
+++ chapter4-20/Ex-342.rkt
@@ -6,22 +6,6 @@
 ; interpretation directions into a directory tree
 (define path1 '("algorithm" ".vscode" "launch.json"))
 
-
-(define (find? d n)
-  (or (find-dirs? (dir-dirs d) n)
-      (find-files? (dir-files d) n)))
-; [List-of Dir] String -> Boolean
-; determines whether a file with name n occurs in dirs
-(define (find-dirs? dirs n)
-  (cond [(empty? dirs) #false]
-        [else (or (find? (first dirs) n)
-                  (find-dirs? (rest dirs) n))]))
-; [List-of Dir] String -> Boolean
-; determines whether a file with name n equals to one of files
-(define (find-files? files n)
-  (cond [(empty? files) #f]
-        [else (or (file-match? (first files) n)
-                  (find-files? (rest files) n))]))
 ; File String -> Boolean
 ; determine if file's name is n
 (define (file-match? file n)
@@ -42,9 +26,14 @@
 ; produces a path to a file with name f in d
 ; otherwise it produces #false.
 (define (find d f)
-  (... (dir-name d) ...
-       (find-dirs (dir-dirs d) f) ....
-       (find-files (dir-files d) f) ...))
+  ; both return #f, means not find, so return #f
+  (if (and (false? (find-dirs (dir-dirs d) f))
+           (false? (find-files (dir-files d) f)))
+      #false
+      ; one of the following have result
+      (if (false? (find-files (dir-files d) f))
+          (cons (dir-name d) (find-dirs (dir-dirs d) f))
+          (list (dir-name d) (find-files (dir-files d) f)))))
 
 (check-expect (find EX "launch.json") path1)
 (check-expect (find EX "dark.js") #false)
