afd25c9e791c90c82873a74c14dcb25bfa85ed7e
starryforce <starryforce@gmail.com>
2023-05-25 22:16:43 +0800

    ex-421

---
+++ chapter5/Ex-421.rkt
@@ -0,0 +1,8 @@
+#lang htdp/isl+
+
+#| Exercise 421.
+Is (bundle '("a" "b" "c") 0) a proper use of the bundle function?
+What does it produce? Why?
+|#
+
+; it's not a proper use, never end, drop always produce same result with input
