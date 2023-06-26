42f52738652d634dc9287f18b16e5e7208ce69fc
starryforce <starryforce@gmail.com>
2023-06-26 18:18:53 +0800

    ex-464

---
+++ chapter5/Ex-464.rkt
@@ -0,0 +1,12 @@
+#lang htdp/isl+
+
+#| Exercise 464. Check that the following system of equations
+has the same solution as the one labeled with (image).
+Again do so by hand and with check-solution from exercise 462.
+|#
+
+#|
+2 x 1 + 2 x 1 + 3 x 2 = 10
+3 x 1 + 9 x 2 = 21
+- 3 x 1 - 8 x 2 = -19
+|# 
