1c0f23e0460d82aeeb4d8e9329698bbebe299b91
starryforce <starryforce@gmail.com>
2023-06-26 18:18:46 +0800

    ex-463

---
+++ chapter5/Ex-463.rkt
@@ -0,0 +1,13 @@
+#lang htdp/isl+
+
+#| Exercise 463.
+Check that the following system of equations has
+the same solution as the one labeled with (image).
+Do so by hand and with check-solution from exercise 462.
+|#
+
+#|
+2 x 1 + 2 x 1 + 3 x 2 = 10
+3 x 1 + 9 x 2 = 21
+1 x 2 = 2
+|#
