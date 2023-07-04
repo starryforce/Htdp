c7ca8bbf19151dc3d42fd85c46938700a0ada573
starryforce <starryforce@gmail.com>
2023-07-04 14:16:58 +0800

    ex-481 part2

--- chapter5/chapter5-29/Ex-481.rkt
+++ chapter5/chapter5-29/Ex-481.rkt
@@ -31,3 +31,15 @@
         [(= (+ (posn-x q1) (posn-y q1)) (+ (posn-x q2) (posn-y q2))) #t]
         [(= (- (posn-x q1) (posn-x q2)) (- (posn-y q1) (posn-y q2))) #t]
         [else #f]))
+
+; [List-of Any] [List-of Any] -> Boolean
+; determines whether l1 & l2 contain the same items—regardless of order
+(define (set=? l1 l2)
+  (cond [(and (empty? l1) (empty? l2)) #t]
+        [else (and (ormap (lambda (x) (equal? (first l1) x)) l2)
+                   (set=? (rest l1) (remove (first l1) l2)))]))
+  
+
+(check-expect (set=? '(1 3 2) '(3 2)) #f)
+(check-expect (set=? '(1 3 2) '(3 2 1)) #t)
+(check-expect (set=? '(1 1 3) '(1 3 3)) #f)
