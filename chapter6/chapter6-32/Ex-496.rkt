73ff58c940cd63e50bcf0767ed4586b9d073aa5f
starryforce <starryforce@gmail.com>
2023-07-12 17:50:33 +0800

    ex-496

---
+++ chapter6/chapter6-32/Ex-496.rkt
@@ -0,0 +1,10 @@
+#lang htdp/isl+
+
+; Exercise 496.
+; What should the value of a be when n0 is 10 and n is 8?
+
+#|
+a is the product of the natural numbers in the interval [n0,n).
+[10 8)
+10 x 9 = 90
+|#
